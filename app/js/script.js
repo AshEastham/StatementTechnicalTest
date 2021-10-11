4614a11140eda9937986e045465acf6d332530c9
AshEastham <ash-eastham@hotmail.co.uk>
2021-10-11 18:02:51 +0100

    Completed cart animation, ready for submission

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,7 @@
+// Get elements
 const bag = document.querySelector(".header__bag");
 const cart = document.querySelector(".cart");
+const cartContainer = document.querySelector(".cart__container");
 const cardCta = document.querySelector(".card__cta");
 const cartTop = document.querySelector(".cart__top");
 const cartClose = document.querySelector(".cart__close");
@@ -30,24 +32,24 @@ overlay.addEventListener("click", function (e) {
 
 // Functions
 function openCart() {
-  cart.style.display = "flex";
-  cart.style.opacity = 1;
   cart.style.width = "100%";
-  cartTop.style.display = "flex";
-  cartTop.style.opacity = 1;
-  cartBottom.style.display = "flex";
-  cartBottom.style.opacity = 1;
+  cartTop.style.width = "100%";
+  cartTop.style.opacity = "1";
+  cartBottom.style.width = "100%";
+  cartBottom.style.opacity = "1";
   overlay.style.display = "block";
   overlay.style.opacity = "1";
+  document.body.style.top = "${window.scrollY}px";
 }
+
 function closeCart() {
-  cart.style.display = "none";
-  cart.style.opacity = 0;
   cart.style.width = "0%";
-  cartTop.style.display = "none";
-  cartTop.style.opacity = 0;
-  cartBottom.style.opacity = 0;
-  cartBottom.style.display = "none";
-  overlay.style.opacity = "0";
+  cartTop.style.width = "0%";
+  cartTop.style.opacity = "0";
+  cartBottom.style.width = "0%";
+  cartBottom.style.opacity = "0";
   overlay.style.display = "none";
+  overlay.style.opacity = "0";
+  document.body.style.position = "";
+  document.body.style.top = "";
 }
